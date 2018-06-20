3bbea41392fdef559c2e0816c000b89caac2aaa4
Preston Pham <ppham@cloudflare.com>
2018-06-20 17:09:33 -0500

    use logfile

--- main.go
+++ main.go
@@ -6,24 +6,39 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
 )
 
 var (
-	host = flag.String("host", "0.0.0.0", "server host")
-	port = flag.String("port", "8080", "server port")
+	host    = flag.String("host", "0.0.0.0", "server host")
+	port    = flag.String("port", "8080", "server port")
+	logfile = flag.String("f", "/tmp/mirror.log", "file path for log file")
 )
 
 func main() {
 	flag.Parse()
 
+	f, err := os.Create(*logfile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	logger := log.New(f, "", log.LstdFlags)
+
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			http.ServeFile(w, r, *logfile)
+			return
+		}
+
 		data, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			log.Println("got err", err)
+			logger.Println("got err", err)
 		}
-		log.Printf("[%s] %s\n", r.Method, data)
+		logger.Printf("[%s] %s\n", r.Method, data)
 	})
 	addr := net.JoinHostPort(*host, *port)
-  log.Println("Listening on", addr)
+	logger.Println("Listening on", addr)
 	http.ListenAndServe(addr, handler)
 }
